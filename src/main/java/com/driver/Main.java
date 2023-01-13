0d88ea6b0398b2f2d080ab5f5f48e2c3cb1db205
Syed Numan <syednuman2000@gmail.com>
2023-01-13 16:15:20 +0530

    Solution 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
         //'name' has private access in 'com.driver.RWOnly'
-        obj.setter("Numan");
-        System.out.println(obj.getter());
+        obj.setName("Numan");
+        System.out.println(obj.getName());
     }
 }
